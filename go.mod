08590988f3dbf43a35629debeb4e3b85ab427fb6
danielewood <daniel@woods.us>
2022-10-11 08:56:55 -0600

    refactoring

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b
 )
 
+require github.com/klauspost/cpuid/v2 v2.1.2
+
 require (
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
--- vanityssh.go
+++ vanityssh.go
@@ -8,6 +8,7 @@ import (
 	"encoding/pem"
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"runtime"
@@ -16,22 +17,23 @@ import (
 	"time"
 
 	"github.com/TylerBrock/colorjson"
+	"github.com/klauspost/cpuid/v2"
 	"github.com/mackerelio/go-osstat/cpu"
 	"github.com/mikesmitty/edkey"
-	"github.com/shopspring/decimal"
 	"golang.org/x/crypto/ed25519"
 	"golang.org/x/crypto/ssh"
 )
 
-var global_user_input string
-var global_user_insensitive bool
-var global_user_streaming bool
-var global_user_fingerprint bool
-var global_user_json bool
-var global_user_json_verbose bool
-var global_user_high_priority bool
-var global_user_update_rate int
-var global_user_threads int
+var flag_regex string
+var flag_forever bool
+var flag_fingerprint bool
+var flag_json bool
+var flag_json_verbose bool
+var flag_json_indent int
+var flag_normal_priority bool
+var flag_benchmark bool
+var flag_update_rate int
+var flag_threads int
 
 // var flagvar int
 var global_counter int64
@@ -40,19 +42,19 @@ var re *regexp.Regexp
 var err error
 
 type json_stats struct {
-	Num_keys    int64 `json:"num_keys"`
-	Elapsed_sec int64 `json:"elapsed_sec"`
-	Rate        int64 `json:"rate"`
-	Cpu_user    int8  `json:"user_cpu_pct"`
+	Num_keys    int64   `json:"num_keys"`
+	Elapsed_sec int64   `json:"elapsed_sec"`
+	Rate        int64   `json:"rate"`
+	Cpu_user    float64 `json:"usage_cpu_user_pct"`
 }
 
 type json_all_stats struct {
-	Num_keys    int64           `json:"num_keys"`
-	Elapsed_sec int64           `json:"elapsed_sec"`
-	Rate        int64           `json:"rate"`
-	Cpu_user    decimal.Decimal `json:"usage_cpu_user_pct"`
-	Cpu_system  decimal.Decimal `json:"usage_cpu_system_pct"`
-	Cpu_idle    decimal.Decimal `json:"usage_cpu_idle_pct"`
+	Num_keys    int64   `json:"num_keys"`
+	Elapsed_sec int64   `json:"elapsed_sec"`
+	Rate        int64   `json:"rate"`
+	Cpu_user    float64 `json:"usage_cpu_user_pct"`
+	Cpu_system  float64 `json:"usage_cpu_system_pct"`
+	Cpu_idle    float64 `json:"usage_cpu_idle_pct"`
 }
 
 type json_keyout struct {
@@ -62,90 +64,53 @@ type json_keyout struct {
 }
 
 func init() {
-	flag.StringVar(&global_user_input, "regex", "", "regex pattern goes here")
-	flag.BoolVar(&global_user_json, "json", false, "json output")
-	flag.BoolVar(&global_user_json_verbose, "morejson", false, "more json output")
-	flag.BoolVar(&global_user_high_priority, "high-priority", false, "sets normal priority, otherwise we use idle priority")
-	flag.BoolVar(&global_user_insensitive, "insensitive", false, "case-insensitive")
-	flag.BoolVar(&global_user_streaming, "streaming", false, "Keep processing keys, even after a match")
-	flag.BoolVar(&global_user_fingerprint, "fingerprint", false, "Match against fingerprint instead of public key")
-	flag.IntVar(&global_user_update_rate, "update-rate", 1, "frequency for updates in seconds")
-	flag.IntVar(&global_user_threads, "threads", 0, "number of threads to use")
+	flag.StringVar(&flag_regex, "regex", "", "regex pattern goes here")
+	flag.BoolVar(&flag_json, "json", false, "json output")
+	flag.BoolVar(&flag_json_verbose, "morejson", false, "more json output")
+	flag.IntVar(&flag_json_indent, "json-indent", 0, "json indentation")
+	flag.BoolVar(&flag_normal_priority, "no-nice", false, "sets normal priority, otherwise we use idle priority")
+	flag.BoolVar(&flag_forever, "streaming", false, "Keep processing keys, even after a match")
+	flag.BoolVar(&flag_fingerprint, "fingerprint", false, "Match against fingerprint instead of public key")
+	flag.BoolVar(&flag_benchmark, "benchmark", false, "Benchmark mode")
+	flag.IntVar(&flag_update_rate, "update-rate", 1, "frequency for updates in seconds")
+	flag.IntVar(&flag_threads, "threads", 0, "number of threads to use")
 	flag.Parse()
-	initTime = time.Now()
-
-	if global_user_insensitive {
-		re, err = regexp.Compile("(?i)" + global_user_input)
-	} else {
-		re, err = regexp.Compile(global_user_input)
-	}
-	if err != nil {
-		os.Exit(1)
-	}
-	fmt.Fprintln(os.Stderr, "global_user_input =", global_user_input)
 }
 
-// func jsonPrintKey(privateKey string, publicKey string, fingerprint string) {
-// 	json_struct := json_keyout{privateKey, publicKey, fingerprint}
-// 	json_bytes, _ := json.Marshal(json_struct)
-// 	var json_obj map[string]interface{}
-// 	json.Unmarshal(json_bytes, &json_obj)
-
-// 	formatter := colorjson.NewFormatter()
-// 	formatter.Indent = 0
-
-// 	json_color_bytes, _ := formatter.Marshal(json_obj)
-// 	fmt.Println(string(json_color_bytes))
-// }
-
-// func jsonPrintBytes(json_bytes []byte) {
-// 	var json_obj map[string]interface{}
-// 	json.Unmarshal(json_bytes, &json_obj)
-
-// 	formatter := colorjson.NewFormatter()
-// 	formatter.Indent = 0
-
-// 	json_color_bytes, _ := formatter.Marshal(json_obj)
-// 	fmt.Println(string(json_color_bytes))
-// }
-
-func jsonPrintStruct(json_struct any) {
+func printJsonStruct(json_struct any, indent int) {
 	json_bytes, _ := json.Marshal(json_struct)
 	var json_obj map[string]interface{}
 	json.Unmarshal(json_bytes, &json_obj)
 
 	formatter := colorjson.NewFormatter()
-	formatter.Indent = 0
+	formatter.Indent = indent
 
 	json_color_bytes, _ := formatter.Marshal(json_obj)
 	fmt.Println(string(json_color_bytes))
 }
 
-func findsshkeys() {
+func findSSHKeys() {
+	matched := false
 	for {
 		global_counter++
 		pubKey, privKey, _ := ed25519.GenerateKey(rand.Reader)
 		publicKey, _ := ssh.NewPublicKey(pubKey)
-		pemKey := &pem.Block{
-			Type:  "OPENSSH PRIVATE KEY",
-			Bytes: edkey.MarshalED25519PrivateKey(privKey),
-		}
-		privateKey := pem.EncodeToMemory(pemKey)
 
-		matched := false
-		if global_user_fingerprint {
+		if flag_fingerprint {
 			matched = re.MatchString(getFingerprint(publicKey))
 		} else {
 			matched = re.MatchString(getAuthorizedKey(publicKey))
 		}
 
 		if matched {
-			if global_user_json || global_user_json_verbose {
-				//jsonPrintKey(string(privateKey), getAuthorizedKey(publicKey), getFingerprint(publicKey))
-				jsonPrintStruct(json_keyout{string(privateKey), getAuthorizedKey(publicKey), getFingerprint(publicKey)})
-				// json_struct := json_keyout{string(privateKey), getAuthorizedKey(publicKey), getFingerprint(publicKey)}
-				// json_bytes, _ := json.Marshal(json_struct)
-				// jsonPrintBytes(json_bytes)
+			pemKey := &pem.Block{
+				Type:  "OPENSSH PRIVATE KEY",
+				Bytes: edkey.MarshalED25519PrivateKey(privKey),
+			}
+			privateKey := pem.EncodeToMemory(pemKey)
+
+			if flag_json || flag_json_verbose {
+				printJsonStruct(json_keyout{string(privateKey), getAuthorizedKey(publicKey), getFingerprint(publicKey)}, flag_json_indent)
 			} else {
 				fmt.Printf("\033[2K\r%s%d", "SSH Keys Processed = ", global_counter)
 				fmt.Println("\nTotal execution time", time.Since(initTime))
@@ -153,12 +118,19 @@ func findsshkeys() {
 				fmt.Printf("%s\n", getAuthorizedKey(publicKey))
 				fmt.Printf("SHA256:%s\n\n", getFingerprint(publicKey))
 			}
-			if !global_user_streaming {
+			if !flag_forever {
 				_ = os.WriteFile("id_ed25519", privateKey, 0600)
 				_ = os.WriteFile("id_ed25519.pub", []byte(getAuthorizedKey(publicKey)), 0644)
 				os.Exit(0)
 			}
 		}
+	}
+}
+
+func benchmark() {
+	for {
+		global_counter++
+		ed25519.GenerateKey(rand.Reader)
 	}
 }
 
@@ -174,19 +146,56 @@ func getAuthorizedKey(key ssh.PublicKey) string {
 	return strings.TrimSpace(string(ssh.MarshalAuthorizedKey(key)))
 }
 
+func printCPUInfo() {
+	info := struct {
+		cpuid.CPUInfo
+		Features []string
+		X64Level int
+	}{
+		CPUInfo:  cpuid.CPU,
+		Features: cpuid.CPU.FeatureSet(),
+		X64Level: cpuid.CPU.X64Level(),
+	}
+	printJsonStruct(info, flag_json_indent)
+}
+
 func main() {
+	if flag_json_verbose {
+		printCPUInfo()
+	}
+
+	if !flag_benchmark {
+		fmt.Fprintln(os.Stderr, "regex =", flag_regex)
+		re, err = regexp.Compile(flag_regex)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error =", err)
+			os.Exit(1)
+		}
+	}
+
 	// default to lower priority
-	if !global_user_high_priority {
+	if !flag_normal_priority {
 		syscall.Setpriority(syscall.PRIO_PROCESS, 0, 20)
 	}
+
+	initTime = time.Now()
+
 	//	input threads, else numcpu
-	if global_user_threads == 0 {
+	if flag_threads == 0 {
 		for i := 1; i <= runtime.NumCPU(); i++ {
-			go findsshkeys()
+			if !flag_benchmark {
+				go findSSHKeys()
+			} else {
+				go benchmark()
+			}
 		}
 	} else {
-		for i := 1; i <= global_user_threads; i++ {
-			go findsshkeys()
+		for i := 1; i <= flag_threads; i++ {
+			if !flag_benchmark {
+				go findSSHKeys()
+			} else {
+				go benchmark()
+			}
 		}
 	}
 
@@ -194,13 +203,13 @@ func main() {
 
 	for {
 
-		if global_user_json || global_user_json_verbose {
+		if flag_json || flag_json_verbose {
 			before, err := cpu.Get()
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%s\n", err)
 				return
 			}
-			time.Sleep(time.Duration(global_user_update_rate) * time.Second)
+			time.Sleep(time.Duration(flag_update_rate) * time.Second)
 
 			after, err := cpu.Get()
 			if err != nil {
@@ -209,27 +218,26 @@ func main() {
 			}
 			total := float64(after.Total - before.Total)
 
-			var cpu_user = decimal.NewFromFloat(float64(after.User-before.User) / total * 100).Round(int32(2))
-			var cpu_system = decimal.NewFromFloat(float64(after.System-before.System) / total * 100).Round(int32(2))
-			var cpu_idle = decimal.NewFromFloat(float64(after.Idle-before.Idle) / total * 100).Round(int32(2))
-
-			var cpu_user_int = int8(float64(after.User-before.User) / total * 100)
+			var cpu_user = math.Floor(float64(after.User-before.User) / total * 100)
+			var cpu_system = math.Floor(float64(after.System-before.System) / total * 100)
+			var cpu_idle = math.Floor(float64(after.Idle-before.Idle) / total * 100)
 
 			var Now = time.Now()
 			var Elapsed = Now.Sub(initTime).Seconds()
 			var Rate = int64(float64(global_counter) / Elapsed)
 
-			if global_user_json_verbose {
-				jsonPrintStruct(json_all_stats{global_counter, int64(Elapsed), Rate, cpu_user, cpu_system, cpu_idle})
+			if flag_json_verbose {
+				printJsonStruct(json_all_stats{global_counter, int64(Elapsed), Rate, cpu_user, cpu_system, cpu_idle}, flag_json_indent)
 			} else {
-				jsonPrintStruct(json_stats{global_counter, int64(Elapsed), Rate, cpu_user_int})
+				printJsonStruct(json_stats{global_counter, int64(Elapsed), Rate, cpu_user}, flag_json_indent)
 			}
-			// json_struct := json_stats{global_counter, int64(Elapsed), Rate}
-			// json_bytes, _ := json.Marshal(json_struct)
-			// jsonPrintBytes(json_bytes)
+
+			if flag_benchmark && Elapsed >= 60 {
+				os.Exit(0)
+			}
 
 		} else {
-			time.Sleep(time.Duration(global_user_update_rate) * time.Second)
+			time.Sleep(time.Duration(flag_update_rate) * time.Second)
 			fmt.Fprintf(os.Stdout, "\033[2K\r%s%d", "SSH Keys Processed = ", global_counter)
 		}
 
